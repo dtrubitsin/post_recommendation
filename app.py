b7399252af04a832836de90449d619249449f70c
Dmitry Trubitsin <132040435+dtrubitsin@users.noreply.github.com>
2024-04-14 21:04:46 +0300

    Оптимизация кода, добавление tf-idf

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import os
 import pandas as pd
-import numpy as np
 
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Integer, Column, Text
-from sqlalchemy.ext.declarative import declarative_base
 from typing import List
 from datetime import datetime
-
 from dotenv import load_dotenv
+from loguru import logger
 
 from schema import PostGet
 
@@ -19,15 +16,6 @@ if __name__ == '__main__':
 
 app = FastAPI()
 
-Base = declarative_base()
-
-
-class Post(Base):
-    __tablename__ = "post"
-    id = Column(Integer, primary_key=True)
-    text = Column(Text)
-    topic = Column(Text)
-
 
 # Путь модели
 def get_model_path(path: str) -> str:
@@ -37,71 +25,90 @@ def get_model_path(path: str) -> str:
         MODEL_PATH = path
     return MODEL_PATH
 
+
 # Загрузка модели
-
-
 def load_models():
     model_path = get_model_path(
-        "/Users/dmitry/Documents/code/Start_ML/module_2/final_project/model/catboost_5_1")
+        "/Users/dmitry/Documents/code/Start_ML/module_2/final_project/model/catboost_1_1")
 
     from_file = CatBoostClassifier()
     model = from_file.load_model(model_path, format='cbm')
     return model
 
 
-# Загрузка данных по чанкам
-def batch_load_sql(query: str) -> pd.DataFrame:
-    CHUNKSIZE = 200000
-    engine = create_engine(os.environ["POSTGRES_CONN"])
-    conn = engine.connect().execution_options(stream_results=True)
-    chunks = []
-    for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
-        chunks.append(chunk_dataframe)
-    conn.close()
-    return pd.concat(chunks, ignore_index=True)
-
-
 # Основная функция загрузки признаков из БД
-def load_features(table: str = 'd_trubitsin_user_feachures_lesson_22') -> pd.DataFrame:
-    data = batch_load_sql(f'SELECT * FROM {table}')
-    return data
+def load_features():
+    # Признаки по постам (созданные)
+    logger.info("loading post feachures")
+    post_feachures = pd.read_sql(
+        """
+        SELECT *
+        FROM d_trubitsin_post_feachures_base 
+        """,
+        con=os.environ["POSTGRES_CONN"]
+    )
+
+    # Признаки по пользователям
+    logger.info("loading user feachures")
+    user_feachures = pd.read_sql(
+        """
+        SELECT *
+        FROM public.user_data
+        """,
+        con=os.environ["POSTGRES_CONN"]
+    )
+    return [post_feachures, user_feachures]
 
 
+logger.info("loading model")
 model = load_models()
-# признаки пользователя
-user_data = load_features('d_trubitsin_user_feachures_lesson_22')
-post_data = load_features(
-    'd_trubitsin_post_feachures_lesson_22')  # признаки постов
-# таблица со всеми постами и исходной информацией
-all_posts = load_features('public.post_text_df')
 
-# эндпоинт для рекомендаций
+logger.info("loading feachures")
+feachures = load_features()
+logger.info("service is up and running")
 
 
+# Эндпоинт для рекомендаций
 @app.get("/post/recommendations/", response_model=List[PostGet])
 def recommended_posts(
         id: int,
         time: datetime,
         limit: int = 5) -> List[PostGet]:
 
-    # создание таблицы с данными для предсказаний
-    request_data = pd.merge(
-        user_data[user_data['user_id'] == id], post_data, how='cross')
+    # Признаки пользователя
+    logger.info(f"user_id: {id}")
+    logger.info("reading feachures")
+    user_feachures = feachures[1].loc[feachures[1]["user_id"] == id].copy()
+    user_feachures.drop('user_id', axis=1, inplace=True)
 
+    # Признаки постов
+    logger.info("dropping columns")
+    post_feachures = feachures[0].drop(["index", "text"], axis=1)
+    content = feachures[0][["post_id", "text", "topic"]]
+
+    # Объединение признаков
+    logger.info("zipping everything")
+    add_user_feachures = dict(
+        zip(user_feachures.columns, user_feachures.values[0]))
+    request_data = post_feachures.assign(**add_user_feachures)
+    request_data = request_data.set_index('post_id')
+
+    # Добавление даты
+    logger.info("adding time info")
     request_data['weekday'] = time.weekday()
     request_data['hour'] = time.hour
 
-    request_data.drop(['user_id', 'post_id'], axis=1, inplace=True)
-
-    # предсказание вероятности для 1 класса (вероятнее всего понравится)
+    # Предсказание вероятности для 1 класса (вероятнее всего понравится)
+    logger.info("predicting")
     probabilities = model.predict_proba(request_data)[:, 1]
+    request_data['prediction'] = probabilities
 
-    # выбор 5 индексов наиболее вероятных постов
-    top_5_indices = np.argsort(probabilities)[::-1][:limit]
-    response = all_posts.iloc[top_5_indices].copy()
+    # Получение топ-5 индексов вероятностей
+    recommended_posts = request_data.sort_values("prediction")[-limit:].index
 
-    # форматирование выдачи согласно шаблону
-    response.rename(columns={'post_id': 'id'}, inplace=True)
-    result_list = response.to_dict(orient='records')
-
-    return result_list
+    return [PostGet(**{
+        'id': i,
+        'text': content[content['post_id'] == i]['text'].values[0],
+        'topic': content[content['post_id'] == i]['topic'].values[0]
+    }) for i in recommended_posts
+    ]
--- schema.py
+++ schema.py
@@ -9,29 +9,3 @@ class PostGet(BaseModel):
 
     class Config:
         orm_mode = True
-
-
-class UserGet(BaseModel):
-    id: int
-    gender: int
-    age: int
-    country: str
-    city: str
-    exp_group: int
-    os: str
-    source: str
-
-    class Config:
-        orm_mode = True
-
-
-class FeedGet(BaseModel):
-    user_id: int
-    post_id: int
-    action: str
-    time: datetime.datetime
-    user: "UserGet"
-    post: "PostGet"
-
-    class Config:
-        orm_mode = True
